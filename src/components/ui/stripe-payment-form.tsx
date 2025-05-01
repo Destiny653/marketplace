12d5bc1490b9780d4f1a71d922c502642c83c937
Destiny653 <fokundem.com@gmail.com>
2025-05-01 19:19:04 +0100

    log

--- src/components/ui/stripe-payment-form.tsx
+++ src/components/ui/stripe-payment-form.tsx
@@ -75,9 +75,7 @@ export function StripePaymentForm({
       "Content-Type": "application/json",
       "X-CC-Version": "2018-03-22"
     }
-  }
-
-  console.log("ConfigCoinbase: ", coinbaseConfig)
+  } 
 
   const isValidUUID = (uuid: string) => {
     return /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(uuid)
